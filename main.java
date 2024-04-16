9371f79b55c0784b53e4bf0fd629881645fad304
Liyaco <134638608+Liyaco@users.noreply.github.com>
2024-04-16 17:48:03 +0200

    Menu de Tres en linia
    Menu simple, sols serveix com a principi, mes tard es fará un de millor

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+        int menuv = 4 ;
+        System.out.println("-------------------\n1-Nova partida    |\n-------------------\n2-Carregar partida|\n-------------------\n3-Configuració    |\n-------------------\n4-Sortir          |\n-------------------");
+        System.out.print("Selecciona l'opció que vols: ");menuv = sc.nextInt();
+        switch (menuv){
+            case 1:
+                System.out.println("Nova partida");
+                break;
+            case 2:
+                System.out.println("Carregar partida");
+                break;
+            case 3:
+                System.out.println("Configuració");
+                break;
+            case 4:
+                System.out.println("Sortir");
+                break;
+        }
+    }
+}

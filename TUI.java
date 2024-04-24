ea75577a44380414c30c30523c19e9012734d238
Marc <masape@elpuig.xeill.net>
2024-04-24 18:55:49 +0200

    Solo queria provar xd

---
+++ TUI.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TUI {
+    //atributs aqui
+}
+
+public static void mostrarMenu(Scanner sc) {
+    int menuv = 4;
+    boolean sortir = false;
+    while (sortir == false) {
+        System.out.println("-------------------" +
+                "\n1-Nova partida    |" +
+                "\n-------------------\n" +
+                "2-Carregar partida|\n" +
+                "-------------------\n" +
+                "3-Configuració    |\n" +
+
+                "-------------------\n" +
+                "4-Sortir          |\n" +
+                "-------------------");
+        System.out.print("Selecciona l'opció que vols: ");
+        menuv = sc.nextInt();
+
+        public static void mostrarTaulell () {
+            throw new NotImplementedException();
+        }
+        public static void recollirJugada () {
+            throw new NotImplementedException();
+        }
+        public static void diDePartida () {
+            throw new NotImplementedException();
+        }
+    }
+}

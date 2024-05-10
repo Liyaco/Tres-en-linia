069eb55cf63a8db5f2c1592d06b28503eadc8d10
Marc <masape@elpuig.xeill.net>
2024-05-10 17:25:47 +0200

    Arreglos

--- src/main/java/Joc.java
+++ src/main/java/Joc.java
@@ -1,18 +1,24 @@
 // La clase Joc //
-public class Joc{
+public class Joc {
     private short torn;
-    public short getTorn() {return torn;}
+
+    public short getTorn() {
+        return torn;
+    }
 
     // Creacio de un array, sera el taulell //
     private char[][] taulell;
-    public char[][] getTaulell() {return taulell;}
+
+    public char[][] getTaulell() {
+        return taulell;
+    }
 
     // El metode novaPartida es l'encarregat de crear una nova partida: //
     // Comença per crear el taulell sobre el atribut taulell de la clase creant-ho. //
     public void novaPartida() {
         this.taulell = new char[3][3];
-        for(int i = 0; i<3;i++){
-            for(int j = 0; j<3;j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 this.taulell[i][j] = '#';
             }
         }
@@ -22,143 +28,21 @@ public class Joc{
     // El metode jugar es l'encarregat de fer correr el joc //
     public boolean jugar(short cordx, short cordy) {
         // Al Main hi farem us d'un iterant per a capacitar el codi recollir unes altres coordenades on es pogui jugar
-        if (this.taulell[cordx][cordy] == '#'){
-            if (this.torn%2 == 1){
+        if (this.taulell[cordx][cordy] == '#') {
+            if (this.torn % 2 == 1) {
                 this.taulell[cordx][cordy] = 'o';
-            }
-            else {
+            } else {
                 this.taulell[cordx][cordy] = 'x';
             }
             this.torn++;
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
     // El metode jugadaGuanyadora es l'encarregat de trobar la jugada guanyadora //
     public static void jugadaGuanyadora() {
-        boolean iguals = false;
-
-        int jugador = 1;
-
-        int contador = 0;
-
-        for(int j = 0; j < 3; j++) {
-        iguals = true;
-
-        for (int i = 1; i < 3; i++) {
-        char anterior = taulell[i - 1][j];
-        char actual = taulell[i][j];
-
-        if (actual == "#"){
-            iguals = false;
-            break;
-        }
-        else
-            if (anterior != actual) {
-                iguals = false;
-                contador++;
-                break;
-            }
-        }
-        }
-        }
-
-        boolean iguals = false;
-
-        int contador = 0;
-
-        for(int j = 0; j < 3; j++) {
-        iguals = true;
-
-        for (int i = 1; i < 3; i++) {
-        char anterior = taulell[i][j - 1];
-        char actual = taulell[i][j];
-
-        if (actual == "#"){
-            iguals = false;
-            break;
-        }
-        else
-            if (anterior != actual) {
-                iguals = false;
-                contador++;
-                break;
-            }
-        }
-        }
-        }
-
-        boolean iguals = false;
-
-        int contador = 0;
-
-        for(int j = 0; j < 3; j++) {
-        iguals = true;
-
-        for (int i = 1; i < 3; i++) {
-        char anterior = taulell[i + 1][j + 1];
-        char actual = taulell[i][j];
-
-        if (actual == "#"){
-            iguals = false;
-            break;
-        }
-        else
-            if (anterior != actual) {
-                iguals = false;
-                contador++;
-                break;
-            }
-        }
-        }
-        }
-
-        boolean iguals = false;
-
-        int contador = 0;
-
-        for(int j = 0; j < 3; j++) {
-        iguals = true;
-
-        for (int i = 1; i < 3; i++) {
-        char anterior = taulell[i - 1][j - 1];
-        char actual = taulell[3][j];
-
-        if (actual == "#"){
-            iguals = false;
-            break;
-        }
-        else{
-            if (anterior != actual) {
-                iguals = false;
-                contador++;
-                break;
-            }
-        }
-        }
-        }
-        }
-
-        if (jugador == 1){
-        //jugador++;
-        }
-
-        else (jugador == 2){
-        //jugador--;
-        }
-
-
-        if (iguals == true){
-        //TUI.fiDePartida();
-        }
-
-        else{
-            Joc.jugar();
-
+        throw new UnsupportedOperationException("Not Soported");
     }
-        }
-            }
 }

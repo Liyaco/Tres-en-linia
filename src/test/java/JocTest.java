1018cecaab133c88887c407cdee6bfb536299148
dam1 <liyac.martinez.ruiz@elpuig.xeill.net>
2024-05-13 15:47:28 +0200

    jugar implemented. jugadaguanyadora fa falta. new method play_game.

--- src/test/java/JocTest.java
+++ src/test/java/JocTest.java
@@ -1,10 +1,13 @@
+import com.sun.source.tree.AssertTree;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 
 class JocTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void creacion () {
         Joc joc = new Joc();
         joc.novaPartida();
@@ -14,6 +17,22 @@ class JocTest {
                 {'#','#','#'}
         };
         Assertions.assertArrayEquals(taulell, joc.getTaulell());
-
+    }
+    @ParameterizedTest
+    @CsvSource(
+            {"0,0",
+            "0,1",
+            "0,2",
+            "1,0",
+            "1,1",
+            "1,2",
+            "2,0",
+            "2,1",
+            "2,2"}
+    )
+    void jugada (short x , short y) {
+        Joc joc = new Joc();
+        joc.novaPartida();
+        Assertions.assertTrue(joc.jugar(x,y));
     }
 }
